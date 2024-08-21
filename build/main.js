51d755ae88befa4bdad06c53b48ba05223c23826
Andrei Alisenok <andreialisenok@gmail.com>
2024-08-21 09:47:42 +0300

    fix project

--- build/main.js
+++ build/main.js
@@ -16,4 +16,30 @@ var swiper = new Swiper('.carousel', {
     prevEl: '.swiper-button-prev'
   }
 });
+var burger = document.querySelector('.burger');
+var nav = document.querySelector('.nav-mobile');
+var body = document.querySelector('body');
+var btnPhones = document.querySelector('.nav-desktop .phones');
+var phonesList = document.querySelector('.nav-desktop .phones__list');
+var btnSearch = document.querySelector('.nav-desktop .search');
+var search = document.querySelector('.search-input');
+var searchClose = document.querySelector('.nav-desktop .search-input__close');
+var menuItem = document.querySelectorAll('.nav-mobile .menu__item');
+burger.addEventListener('click', function () {
+  burger.classList.toggle('active');
+  nav.classList.toggle('active');
+  body.classList.toggle('is-lock');
+});
+btnPhones.addEventListener('click', function () {
+  phonesList.classList.toggle('active');
+});
+btnSearch.addEventListener('click', function () {
+  search.classList.add('active');
+});
+searchClose.addEventListener('click', function () {
+  search.classList.remove('active');
+});
+menuItem.forEach(function (item) {
+  item.lastElementChild.classList.contains('submenu') ? item.firstElementChild.classList.add('arrow') : '';
+});
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -14,3 +14,33 @@ const swiper = new Swiper('.carousel', {
     prevEl: '.swiper-button-prev',
   },
 });
+
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav-mobile');
+const body = document.querySelector('body');
+const btnPhones = document.querySelector('.nav-desktop .phones');
+const phonesList = document.querySelector('.nav-desktop .phones__list');
+const btnSearch = document.querySelector('.nav-desktop .search');
+const search = document.querySelector('.search-input');
+const searchClose = document.querySelector('.nav-desktop .search-input__close');
+const menuItem = document.querySelectorAll('.nav-mobile .menu__item');
+
+burger.addEventListener('click', () => {
+  burger.classList.toggle('active');
+  nav.classList.toggle('active');
+  body.classList.toggle('is-lock');
+});
+btnPhones.addEventListener('click', () => {
+  phonesList.classList.toggle('active');
+});
+btnSearch.addEventListener('click', () => {
+  search.classList.add('active');
+});
+searchClose.addEventListener('click', () => {
+  search.classList.remove('active');
+});
+menuItem.forEach((item) => {
+  item.lastElementChild.classList.contains('submenu')
+    ? item.firstElementChild.classList.add('arrow')
+    : '';
+});
