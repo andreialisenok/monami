cd85eebeb2f4cc749653b09367cfc6325ff0c773
Andrei Alisenok <andreialisenok@gmail.com>
2024-08-28 11:06:05 +0300

    fix products

--- build/main.js
+++ build/main.js
@@ -16,19 +16,44 @@ var swiper = new Swiper('.carousel', {
     prevEl: '.swiper-button-prev'
   }
 });
+var carouselProduct = new Swiper('.carousel-product', {
+  loop: true,
+  spaceBetween: 5,
+  slidesPerView: 5,
+  freeMode: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  }
+});
+var carouselProduct2 = new Swiper('.carousel-product2', {
+  loop: true,
+  spaceBetween: 10,
+  thumbs: {
+    swiper: carouselProduct
+  }
+});
 var burger = document.querySelector('.burger');
-var nav = document.querySelector('.nav-mobile');
+var menumobile = document.querySelector('#menumobile');
 var body = document.querySelector('body');
-var btnPhones = document.querySelector('.nav-desktop .phones');
-var phonesList = document.querySelector('.nav-desktop .phones__list');
-var btnSearch = document.querySelector('.nav-desktop .search');
-var search = document.querySelector('.search-input');
-var searchClose = document.querySelector('.nav-desktop .search-input__close');
-var menuItem = document.querySelectorAll('.nav-mobile .menu__item');
+var btnPhones = document.querySelector('#btnPhones');
+var phonesList = document.querySelector('#btnPhones .phones__list');
+var btnSearch = document.querySelector('#btnSearch');
+var search = document.querySelector('#searchInput');
+var searchClose = document.querySelector('#searchInput .search-input__close');
+var menuItem = document.querySelectorAll('#menumobile .menu .menu__item');
+var submenuItem = document.querySelectorAll('#menumobile .submenu .submenu__item');
+var backArrows = document.querySelectorAll('.backArrow');
+var mobileOverlay = document.querySelector('.mobile-overlay');
+// const submenuItem = document.querySelectorAll(
+//   '#menumobile .submenu .menu__item'
+// );
+
 burger.addEventListener('click', function () {
   burger.classList.toggle('active');
-  nav.classList.toggle('active');
+  menumobile.classList.toggle('active');
   body.classList.toggle('is-lock');
+  mobileOverlay.classList.toggle('active');
 });
 btnPhones.addEventListener('click', function () {
   phonesList.classList.toggle('active');
@@ -40,6 +65,29 @@ searchClose.addEventListener('click', function () {
   search.classList.remove('active');
 });
 menuItem.forEach(function (item) {
-  item.lastElementChild.classList.contains('submenu') ? item.firstElementChild.classList.add('arrow') : '';
+  if (item.children.length === 2) {
+    item.children[0].classList.add('arrow');
+    item.addEventListener('click', function (e) {
+      e.preventDefault();
+      item.children[1].classList.add('active');
+    });
+    backArrows.forEach(function (link) {
+      link.addEventListener('click', function (e) {
+        e.preventDefault();
+        item.children[1].classList.remove('active');
+      });
+    });
+  }
+});
+submenuItem.forEach(function (item) {
+  item.addEventListener('click', function (e) {
+    e.stopPropagation();
+  });
+});
+mobileOverlay.addEventListener('click', function (e) {
+  burger.classList.toggle('active');
+  menumobile.classList.toggle('active');
+  body.classList.toggle('is-lock');
+  mobileOverlay.classList.toggle('active');
 });
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -15,20 +15,47 @@ const swiper = new Swiper('.carousel', {
   },
 });
 
+const carouselProduct = new Swiper('.carousel-product', {
+  loop: true,
+  spaceBetween: 5,
+  slidesPerView: 5,
+  freeMode: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+const carouselProduct2 = new Swiper('.carousel-product2', {
+  loop: true,
+  spaceBetween: 10,
+  thumbs: {
+    swiper: carouselProduct,
+  },
+});
+
 const burger = document.querySelector('.burger');
-const nav = document.querySelector('.nav-mobile');
+const menumobile = document.querySelector('#menumobile');
 const body = document.querySelector('body');
-const btnPhones = document.querySelector('.nav-desktop .phones');
-const phonesList = document.querySelector('.nav-desktop .phones__list');
-const btnSearch = document.querySelector('.nav-desktop .search');
-const search = document.querySelector('.search-input');
-const searchClose = document.querySelector('.nav-desktop .search-input__close');
-const menuItem = document.querySelectorAll('.nav-mobile .menu__item');
+const btnPhones = document.querySelector('#btnPhones');
+const phonesList = document.querySelector('#btnPhones .phones__list');
+const btnSearch = document.querySelector('#btnSearch');
+const search = document.querySelector('#searchInput');
+const searchClose = document.querySelector('#searchInput .search-input__close');
+const menuItem = document.querySelectorAll('#menumobile .menu .menu__item');
+const submenuItem = document.querySelectorAll(
+  '#menumobile .submenu .submenu__item'
+);
+const backArrows = document.querySelectorAll('.backArrow');
+const mobileOverlay = document.querySelector('.mobile-overlay');
+// const submenuItem = document.querySelectorAll(
+//   '#menumobile .submenu .menu__item'
+// );
 
 burger.addEventListener('click', () => {
   burger.classList.toggle('active');
-  nav.classList.toggle('active');
+  menumobile.classList.toggle('active');
   body.classList.toggle('is-lock');
+  mobileOverlay.classList.toggle('active');
 });
 btnPhones.addEventListener('click', () => {
   phonesList.classList.toggle('active');
@@ -40,7 +67,28 @@ searchClose.addEventListener('click', () => {
   search.classList.remove('active');
 });
 menuItem.forEach((item) => {
-  item.lastElementChild.classList.contains('submenu')
-    ? item.firstElementChild.classList.add('arrow')
-    : '';
+  if (item.children.length === 2) {
+    item.children[0].classList.add('arrow');
+    item.addEventListener('click', (e) => {
+      e.preventDefault();
+      item.children[1].classList.add('active');
+    });
+    backArrows.forEach((link) => {
+      link.addEventListener('click', (e) => {
+        e.preventDefault();
+        item.children[1].classList.remove('active');
+      });
+    });
+  }
+});
+submenuItem.forEach((item) => {
+  item.addEventListener('click', (e) => {
+    e.stopPropagation();
+  });
+});
+mobileOverlay.addEventListener('click', (e) => {
+  burger.classList.toggle('active');
+  menumobile.classList.toggle('active');
+  body.classList.toggle('is-lock');
+  mobileOverlay.classList.toggle('active');
 });
